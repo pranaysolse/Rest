cd0664b18620e97d047ebc32b187696857f4fcae
unknown <pranay solse>
2019-09-25 20:44:15 +0530

    latest

--- index.js
+++ index.js
@@ -2,18 +2,35 @@
 var express = require("express");
 var bodyparser = require("body-parser");
 var app = express();
+var request = require("request");
 require("dotenv").config();
 const api_key = process.env.API_KEY;
 app.use(bodyparser.json());
-var request = require("request");
-var data = request.get(`https://api.data.gov.in/resource/3b01bcb8-0b14-4abf-b6f2-c1bfd384ba69?api-key=${api_key}&format=json&offset=0&limit=10`, (error, response, body) =>{
+
+let air_data = request.get(`https://api.data.gov.in/resource/3b01bcb8-0b14-4abf-b6f2-c1bfd384ba69?api-key=${api_key}&format=json&offset=0&limit=10`, (error, response, body) =>{
     if(error) {
-        return console.log(error);
+        return console.log("error");
     }
-    console.dir(JSON.parse(body));
+    //console.dir(JSON.parse(body));
     return JSON.parse(body);
-
-
  });
 
-console.log(api_key);
+//  var mandi_data = request.get(`https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070?api-key=${api_key}&format=json&offset=0&limit=10`, (error, response, body)=>{
+//      if(error) {
+//          return console.log("error");
+//      }
+//      return JSON.parse(body);
+//  })
+
+//  var mandi_data_1 = request.get(`https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070?api-key=579b464db66ec23bdd000001f8b3ce73c1df400a799e1878f48b18f0a&format=json&offset=0&limit=10`, (error, response, body)=>{
+//      if(error) {
+//          return console.log("error");
+//      }
+//      return JSON.parse(body);
+//  });
+
+
+// console.log(mandi_data_1);
+  console.log(air_data);
+
+// console.log(api_key);
